ac6965a69d8d96a13e2b0ec109a4af3161dea9a4
ЛавреновМВ <@PC-05>
2020-02-03 16:59:16 +1000

    Разработка

---
+++ TimetableScreen/Models/NetworkTransport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimetableScreen.Models
+{
+    class NetworkTransport
+    {
+
+        public void Send(object obj)
+        {
+
+        }
+        public object Recieve()
+        {
+            return null;
+        }
+    }
+}
--- TimetableScreen/ShellView.xaml.cs
+++ TimetableScreen/ShellView.xaml.cs
@@ -18,9 +18,9 @@ namespace TimetableScreen
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ShellView : Window
     {
-        public MainWindow()
+        public ShellView()
         {
             InitializeComponent();
         }
